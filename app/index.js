43166ca437c07eed1344d2ded1fc3c304ad54e10
Abdullahi Shobaloju <sabdullahi@cinnsol.com>
2024-03-12 13:27:11 +0100

    homepage initiated

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+    return(
+        <View>
+            <Text>THIS IS THE HOMEPAGE</Text>
+        </View>
+    )
+}
+
+export default Home
