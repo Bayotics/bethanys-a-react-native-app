9e18a1c02ec90feb1ec5841b9947f55a32755a65
Abdullahi Shobaloju <sabdullahi@cinnsol.com>
2024-03-15 12:16:01 +0100

    icons and header done

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 import { View, Text } from "react-native";
+import Header from "./Header";
 
 const Home = () => {
     return(
         <View>
+            <Header />
             <Text>THIS IS THE HOMEPAGE</Text>
         </View>
     )
